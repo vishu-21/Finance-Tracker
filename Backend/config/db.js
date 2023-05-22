081220d82934ba4daa2340df20660289ce66f688
vishu-21 <vishal211199@gmail.com>
2023-05-22 17:22:05 +0530

    added mongo

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() =>{
 try {
-    const conn =  await mongoose.connect(process.env.MONGO_URI);
+    const conn =  await mongoose.connect("mongodb+srv://Vishal:tE48Z9nxe4SGoBeB@vishal.cinsd5w.mongodb.net/data?retryWrites=true&w=majority");
 
         console.log(`MonngoDB connected: ${conn.connection.host}`.green.bold);
     } catch (error) {
